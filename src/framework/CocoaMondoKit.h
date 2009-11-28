5ecd7ab30e1b41500dda785778d8602dac3d884b
Matthieu Cormier <mcormier@cs.dal.ca>
2009-11-28 14:38:29 -0400

    Created convenience header.
    This will be more useful when there is more than one
    UI component in the plugin.

---
+++ src/framework/CocoaMondoKit.h
@@ -0,0 +1,10 @@
+/*
+ *  CocoaMondoKit.h
+ *  CocoaMondoKit
+ *
+ *  Created by Matthieu Cormier on 11/28/09.
+ *  Copyright 2009 Allusions Software. All rights reserved.
+ *
+ */
+
+#import <CocoaMondoKit/MondoTextField.h>
