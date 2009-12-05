77e625b10f48a534db1f47b2d79b8ffa1dce7c1a
Matthieu Cormier <mcormier@cs.dal.ca>
2009-12-05 16:46:55 -0400

    Fix copyright notice.

--- src/framework/mondoTextField/MondoTextFieldCell.h
+++ src/framework/mondoTextField/MondoTextFieldCell.h
@@ -1,6 +1,4 @@
-//  Created by Matthieu Cormier on Mon December 8 2008.
-//
-//  Copyright (c) 2008 Matthieu Cormier. All rights reserved.
+//  Copyright (c) MMIX Matthieu Cormier. All rights reserved.
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided 
 //  that the following conditions are met:
 //
@@ -18,7 +16,6 @@
 //  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
 //  WHETHER IN CONTRACT,  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 #import <Cocoa/Cocoa.h>
 
 
--- src/framework/mondoTextField/MondoZoomWindowController.m
+++ src/framework/mondoTextField/MondoZoomWindowController.m
@@ -1,6 +1,4 @@
-//  Created by Matthieu Cormier on Mon December 8 2008.
-//
-//  Copyright (c) 2008 Matthieu Cormier. All rights reserved.
+//  Copyright (c) MMIX Matthieu Cormier. All rights reserved.
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided 
 //  that the following conditions are met:
 //
