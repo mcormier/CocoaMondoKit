63b7da6a56e7064396dd7d1e25d670c097b89df7
Matthieu Cormier <mcormier@cs.dal.ca>
2009-12-27 19:33:08 -0400

    Override some methods for IB.
    Fixes the dragging and drawing bugs in Interface Builder.

--- src/Plugin/MondoSwitchIntegration.m
+++ src/Plugin/MondoSwitchIntegration.m
@@ -1,8 +1,52 @@
 
 #import <InterfaceBuilderKit/InterfaceBuilderKit.h>
 #import "MondoSwitch.h"
+#import "MondoSwitch_Private.h"
+
+@class IBApplication;
+
+@interface MondoSwitch (MondoSwitchIntegration)
+-(BOOL)isRunningInIB;
+@end
+
 
 @implementation MondoSwitch ( MondoSwitchIntegration) 
+
+-(BOOL)isRunningInIB {
+  NSString *appName = NSStringFromClass([[NSApplication sharedApplication] class]);
+  return [appName caseInsensitiveCompare:@"IBApplication"] == NSOrderedSame;  
+  
+}
+
+
+// Override awakeFromNib.  This fixes a dragging bug
+// by turning off core data when in Interface Builder.
+- (void)awakeFromNib { 
+  if (![self isRunningInIB]) {
+    [self setupLayers];
+  }
+}
+
+// Override drawing behaviour in Interface builder.
+// Interface builder doesn't seem to work well with Core Animation layers
+// So draw a simple switch using an image.
+- (void)drawRect:(NSRect)dirtyRect {
+  
+  // For the case when we are in Interface Builder
+  // but the Cocoa Simulator is being used.
+  if (![self isRunningInIB]) {
+    [self coreAnimationDrawRect:dirtyRect];
+    return;
+  }
+  
+  Class mondoKitClazz = NSClassFromString(@"CocoaMondoKit");
+  NSBundle *otherBundle = [NSBundle bundleForClass:mondoKitClazz];
+  NSImage *image = [[NSImage alloc] initWithContentsOfFile:[otherBundle pathForImageResource:@"MondoSwitchIBDisplay.png"]];
+
+  [image drawInRect:dirtyRect fromRect:NSZeroRect            
+         operation:NSCompositeSourceOver fraction:1.0]; 
+  [image release];
+}
 
 
 @end
